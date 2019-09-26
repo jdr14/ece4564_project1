5bf5cd572f95203f315337113c966245c6c425b4
Sulaiman Panjshiri <sulaip1@vt.edu>
2019-09-25 23:48:51 -0400

    starting to parse agrguments in client

--- client.py
+++ client.py
@@ -1,15 +1,34 @@
 # Import the necessary package to process data in JSON format
-from client_keys import *
+from ClientKeys import *
+import socket
+# Import the tweepy library
+import tweepy
+import hashlib
+import _pickle
+import sys
+from cryptography.fernet import Fernet
+
+parseArguments = argparse.ArgumentParser(description="Reads through command line arguments")
+
+# This code was taken from this tutorial on tweepy (No author)
+# http://docs.tweepy.org/en/latest/streaming_how_to.html 
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print("The question tweet: " + str(status.text))
+
+
+    def on_error(self, status):
+        print(status)
+
 try:
     import json
 except ImportError:
     import simplejson as json
 
-# Import the tweepy library
-import tweepy
 
 # Setup tweepy to authenticate with Twitter credentials:
-
+# This code was made by Wei Xu at http://socialmedia-class.org/twittertutorial.html
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
@@ -26,8 +45,8 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, 
 # This is the equivalent of /timeline/home on the Web.
 #---------------------------------------------------------------------------------------------------------------------
 
-for status in tweepy.Cursor(api.home_timeline).items(200):
-	print(status._json)
+# for status in tweepy.Cursor(api.home_timeline).items(200):
+# 	print(status._json)
 	
 #---------------------------------------------------------------------------------------------------------------------
 # Twitter API development use pagination for Iterating through timelines, user lists, direct messages, etc. 
--- server.py
+++ server.py
@@ -1,1 +1,1 @@
-from server_keys import *
+from ServerKeys import *
