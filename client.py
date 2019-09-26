93d25b9a56440ce6d141dc5c5eebe0b225b577ca
Sulaiman Panjshiri <sulaip1@vt.edu>
2019-09-26 09:17:06 -0400

    changed parse args

--- client.py
+++ client.py
@@ -12,10 +12,9 @@ from cryptography.fernet import Fernet
 # Argparse is the recommended parsing library in Python provided by the python docs
 # https://docs.python.org/3/howto/argparse.html
 parseArguments = argparse.ArgumentParser(description="Reads through command line arguments")
-parseArguments.add_argument("-s", help="Assign IP Address of Server")
-parseArguments.add_argument("-p",help="Assign Port Number of Server")
+parseArguments.add_argument("-sip", help="Assign IP Address of Server")
+parseArguments.add_argument("-sp",help="Assign Port Number of Server")
 parseArguments.add_argument("-z",help="Assign Socket Size of Server")
-parseArguments.add_argument("-t",help="Assign Hashtag Needed to See")
 parsedArguments = parseArguments.parse_args()
 
 if parsedArguments.s != "-s":
@@ -24,8 +23,6 @@ if parsedArguments.p != "-p":
     parseArguments.error("You must specify a valid Port Number of Server")
 if parsedArguments.z != "-z":
     parseArguments.error("You must specify a valid Socket Size of Server")
-if parsedArguments.t != "-t":
-    parseArguments.error("You must specify a valid Hashtag Need to See")
 
 # This code was taken from this tutorial on tweepy (No author)
 # http://docs.tweepy.org/en/latest/streaming_how_to.html 
