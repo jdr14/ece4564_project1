517b48dd2d7819c9d5106a0052e3073b489e9890
Sulaiman Panjshiri <sulaip1@vt.edu>
2019-09-25 20:23:20 -0400

    added twitter api keys

---
+++ client_keys.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = '3190786043-SSvDKjJZBtFw2yC64HOlade3AUC18thW2O2cuQc'
+ACCESS_SECRET = 'VOndBmNyoYJ3DIQRFGnbHCEPrZt7JQvrioW2eDujbLkVp'
+CONSUMER_KEY = '2jUAOcmZ5Rb4Cigyv7VloGbhw'
+CONSUMER_SECRET = 'YXBvTuTrBY3d6RjdHIl9oHJYkwVcTQv129XpyiySVZBbUGtn40'
