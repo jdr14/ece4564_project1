96b8b59ceda1ef8d4773c4af7f3803935a217d4d
jovany <jovanyc4@vt.edu>
2019-09-28 15:29:14 -0400

    server has parsed through the message and has validated the hashes. needs api calls

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import hashlib
 import _pickle
 import sys
 import argparse
+from cryptography.fernet import Fernet
 
 parseArguments = argparse.ArgumentParser(description="Read through command line arguemnts")
 parseArguments.add_argument("-sp", help="Assign Port Number on Server")
@@ -20,10 +21,33 @@ socketSize = parsedArguments.z
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
         serverSocket.bind((socket.gethostname(), int(portNumber)))
+        print("[Checkpoint 01] Connecting to port {} and waiting for message".format(portNumber))
         serverSocket.listen()
         conn, addr = serverSocket.accept()
         while True:
-                print("connected by: ", addr)
-                data = conn.recv(16)
-                print('received {!r}'.format(data))
+                print("[Checkpoint 02] Recieved message, connected by: ", addr)
+                # print("connected by: ", addr)
+                data = conn.recv(int(socketSize))
+                dataUnpickled = _pickle.loads(data)
+                # print('received: {}'.format(dataUnpickled))
+                keyCollected = dataUnpickled[0]
+                print("newKey: ", keyCollected)
+                originalEncryptedMessage = dataUnpickled[1]
+                # print("newMessage: ", originalEncryptedMessage)
+                hashCollected = dataUnpickled[2]
+                # print("newHash: ", hashCollected)
+                f= Fernet(keyCollected)
+                decryptedBytes = f.decrypt(originalEncryptedMessage)
+                decryptedString = str(decryptedBytes)
+                index1 = decryptedString.find('"')+1
+                index2 = decryptedString.find('"', index1)
+                decryptedQuestion = decryptedString[index1:index2]
+                print("decrypt: {}".format(decryptedQuestion))
+
+                newH = hashlib.md5()
+                newH.update(originalEncryptedMessage)
+                newMd5Hash = newH.hexdigest()
+                # print("md5newHash: ", newMd5Hash)
+                if(newMd5Hash == hashCollected):
+                        print("[Checkpoint 04] Hashes have been validated.")
                 break
